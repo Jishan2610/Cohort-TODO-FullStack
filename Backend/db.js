4be0e44e91759be0374631f550492f3a2e825199
Jishan2610 <75787027+Jishan2610@users.noreply.github.com>
2024-08-12 14:36:35 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 const { boolean } = require("zod");
-//mongodb+srv://Jishan:Sja_2610@nodeexpress-projects.tfpkulu.mongodb.net/TODOS
-mongoose.connect("mongodb+srv://Jishan:Sja_2610@nodeexpress-projects.tfpkulu.mongodb.net/TODOS")
+
+mongoose.connect("Your Connection String")
 
 const todoSchema=mongoose.Schema({
     title:String,
